6cac9bc5c6d7e5b2f53cdcaa47c15c45ba5c4d97
carvoxcc <carvoxcc@gmail.com>
2026-07-21 22:48:18 +0530

    third update

--- js/main.js
+++ js/main.js
@@ -10,56 +10,80 @@
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
 
-    // Sticky Navbar
-    $(window).scroll(function () {
+    // Sticky Navbar & Auto-close open mobile navbar list on scroll
+    $(window).on('scroll touchmove', function () {
         if ($(this).scrollTop() > 300) {
             $('.sticky-top').css('top', '0px');
         } else {
             $('.sticky-top').css('top', '-100px');
         }
+
+        // Auto-close open mobile navbar when scrolling/swiping the page
+        if ($('.navbar-collapse').hasClass('show')) {
+            $('.navbar-collapse').removeClass('show');
+            $('.navbar-toggler').addClass('collapsed').attr('aria-expanded', 'false');
+        }
     });
-    
-    
+
+    // Auto-close mobile navbar on link click or outside click
+    $('.navbar-nav .nav-link:not(.dropdown-toggle), .navbar-nav .dropdown-item, .navbar .btn').on('click', function () {
+        if ($('.navbar-collapse').hasClass('show')) {
+            $('.navbar-collapse').removeClass('show');
+            $('.navbar-toggler').addClass('collapsed').attr('aria-expanded', 'false');
+        }
+    });
+
+    $(document).on('click touchstart', function (e) {
+        var $navbar = $('.navbar');
+        if (!$navbar.is(e.target) && $navbar.has(e.target).length === 0 && $('.navbar-collapse').hasClass('show')) {
+            $('.navbar-collapse').removeClass('show');
+            $('.navbar-toggler').addClass('collapsed').attr('aria-expanded', 'false');
+        }
+    });
+
+
     // Dropdown on mouse hover
     const $dropdown = $(".dropdown");
     const $dropdownToggle = $(".dropdown-toggle");
     const $dropdownMenu = $(".dropdown-menu");
     const showClass = "show";
-    
-    $(window).on("load resize", function() {
+
+    $(window).on("load resize", function () {
         if (this.matchMedia("(min-width: 992px)").matches) {
             $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
+                function () {
+                    const $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "true");
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function () {
+                    const $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "false");
+                    $this.find($dropdownMenu).removeClass(showClass);
+                }
             );
         } else {
             $dropdown.off("mouseenter mouseleave");
         }
     });
-    
-    
-    // Back to top button
+
+
+    // Back to top button & social media icons scroll toggle
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
+            $('.rightSideFixedSocialIcons').addClass('show-on-scroll');
         } else {
             $('.back-to-top').fadeOut('slow');
+            $('.rightSideFixedSocialIcons').removeClass('show-on-scroll');
         }
     });
     $('.back-to-top').click(function (e) {
@@ -93,19 +117,19 @@
         margin: 25,
         dots: true,
         loop: true,
-        nav : false,
+        nav: false,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             }
         }
     });
-    
+
 })(jQuery);
 
